6b570b20ae562354d5dd0b89ee420f1116e98f95
koraynilay <koray.fra@gmail.com>
2025-06-30 19:56:07 +0200

    code.js: fix formatting

--- code.js
+++ code.js
@@ -54,30 +54,30 @@ function download(data, filename, type) {
 
 		while (has_next) {
 			await fetch(
-	`https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=` +
-		encodeURIComponent(
-			JSON.stringify({
-				id: userId,
-				include_reel: true,
-				fetch_mutual: true,
-				first: 50,
-				after: after,
-			})
-		)
+				`https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=` +
+				encodeURIComponent(
+					JSON.stringify({
+						id: userId,
+						include_reel: true,
+						fetch_mutual: true,
+						first: 50,
+						after: after,
+					})
+				)
 			)
-	.then((res) => res.json())
-	.then((res) => {
-		has_next = res.data.user.edge_followed_by.page_info.has_next_page;
-		after = res.data.user.edge_followed_by.page_info.end_cursor;
-		followers = followers.concat(
-			res.data.user.edge_followed_by.edges.map(({ node }) => {
-				return {
-		username: node.username,
-		full_name: node.full_name,
-				};
-			})
-		);
-	});
+			.then((res) => res.json())
+			.then((res) => {
+				has_next = res.data.user.edge_followed_by.page_info.has_next_page;
+				after = res.data.user.edge_followed_by.page_info.end_cursor;
+				followers = followers.concat(
+					res.data.user.edge_followed_by.edges.map(({ node }) => {
+						return {
+							username: node.username,
+							full_name: node.full_name,
+						};
+					})
+				);
+			});
 		}
 
 		console.log({ followers });
@@ -87,37 +87,37 @@ function download(data, filename, type) {
 
 		while (has_next) {
 			await fetch(
-	`https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=` +
-		encodeURIComponent(
-			JSON.stringify({
-				id: userId,
-				include_reel: true,
-				fetch_mutual: true,
-				first: 50,
-				after: after,
-			})
-		)
+				`https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=` +
+				encodeURIComponent(
+					JSON.stringify({
+						id: userId,
+						include_reel: true,
+						fetch_mutual: true,
+						first: 50,
+						after: after,
+					})
+				)
 			)
-	.then((res) => res.json())
-	.then((res) => {
-		has_next = res.data.user.edge_follow.page_info.has_next_page;
-		after = res.data.user.edge_follow.page_info.end_cursor;
-		followings = followings.concat(
-			res.data.user.edge_follow.edges.map(({ node }) => {
-				return {
-		username: node.username,
-		full_name: node.full_name,
-				};
-			})
-		);
-	});
+			.then((res) => res.json())
+			.then((res) => {
+				has_next = res.data.user.edge_follow.page_info.has_next_page;
+				after = res.data.user.edge_follow.page_info.end_cursor;
+				followings = followings.concat(
+					res.data.user.edge_follow.edges.map(({ node }) => {
+						return {
+							username: node.username,
+							full_name: node.full_name,
+						};
+					})
+				);
+			});
 		}
 
 		console.log({ followings });
 
 		dontFollowMeBack = followings.filter((following) => {
 			return !followers.find(
-	(follower) => follower.username === following.username
+				(follower) => follower.username === following.username
 			);
 		});
 
@@ -125,14 +125,14 @@ function download(data, filename, type) {
 
 		iDontFollowBack = followers.filter((follower) => {
 			return !followings.find(
-	(following) => following.username === follower.username
+				(following) => following.username === follower.username
 			);
 		});
 
 		console.log({ iDontFollowBack });
 
 		console.log(
-			`Process is done: Type 'copy(followers)' or 'copy(followings)' or 'copy(dontFollowMeBack)' or 'copy(iDontFollowBack)' in the console and paste it into a text editor to take a look at it'`
+			`Process is done: Type 'copy(followers)' or 'copy(followings)' or 'copy(dontFollowMeBack)' or 'copy(iDontFollowBack)' in the console and paste it into a text editor to take a look at it`
 		);
 
 		let filename = "";
